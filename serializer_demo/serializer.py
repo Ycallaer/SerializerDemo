b14437ebb769d8361481dd178f8c011de5a24b14
YYC <yyc@novozymes.com>
2019-03-29 15:36:05 +0100

    Adding protobuf code to the repo

--- serializer_demo/serializer.py
+++ serializer_demo/serializer.py
@@ -1,10 +1,13 @@
 import pandas
 from serializer_demo.thriftify.thrift_dow_jones import DowJonesThrift
 from serializer_demo.timer_function.timer_decorator import timing
+from serializer_demo.protobuf.proto_dow_jones import DownJonesProto
+
 
 @timing
 def main_pickle(pd_df):
     pd_df.to_pickle(path='resources/dow_jones_index.pickle')
+
 
 @timing
 def main_thrift(pd_df):
@@ -14,7 +17,17 @@ def main_thrift(pd_df):
     result = thriftify.serialize_message()
     print(result)
 
+
+@timing
+def main_protobuf(pd_df):
+    protobuf = DownJonesProto()
+    pd_df.apply(protobuf.create_proto_msg, axis=1)
+    result = protobuf.create_result()
+    print(result)
+
+
 if __name__=="__main__":
     dow_jones_data = pandas.read_csv(filepath_or_buffer="resources/dow_jones_index.data", sep=',')
-    print(main_thrift(dow_jones_data))
-    print(main_pickle(dow_jones_data))
+    #print(main_thrift(dow_jones_data))
+    #print(main_pickle(dow_jones_data))
+    print(main_protobuf(dow_jones_data))
